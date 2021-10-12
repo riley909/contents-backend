43b5796ab13ef6c6bad457fce85c0e3aaa3ebc1e
riley909 <increaseblue666@gmail.com>
2021-10-12 21:26:46 +0900

    add ConflictException

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Episode } from 'contents-backend';
 import { ContentsService } from 'src/contents/contents.service';
 
@@ -17,6 +17,10 @@ export class EpisodesService {
 
   buyEpisode(content_id: string, episode_id: string): Episode {
     const episode = this.getEpisodeById(content_id, episode_id);
+
+    if (episode.isBuy === true) {
+      throw new ConflictException('이미 구매한 에피소드입니다.');
+    }
     episode.isBuy = true;
     return episode;
   }
