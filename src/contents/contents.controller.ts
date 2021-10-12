726b6759aafbd9ecc68b286196223cd6f3b3f1ac
riley909 <increaseblue666@gmail.com>
2021-10-12 18:10:34 +0900

    add updateContent

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { Content } from 'contents-backend';
 import { ContentsService } from './contents.service';
 import { GetContentsFilterDto } from './dto/get-contents-filter.dto';
+import { UpdateContentDto } from './dto/update-content.dto';
 
 @Controller('contents')
 export class ContentsController {
@@ -15,5 +16,13 @@ export class ContentsController {
   @Get('/:id')
   getContentById(@Param('id') id: string): Content {
     return this.contentsService.getContentById(id);
+  }
+
+  @Patch('/:id')
+  updateContent(
+    @Param('id') id: string,
+    @Body() updateContentDto: UpdateContentDto,
+  ): Content {
+    return this.contentsService.updateContent(id, updateContentDto);
   }
 }
