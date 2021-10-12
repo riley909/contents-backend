bd6ccc4274727aac718a8bbaf7bdf5e2b71f331e
riley909 <increaseblue666@gmail.com>
2021-10-12 21:13:59 +0900

    add getEpisodeById

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -6,6 +6,14 @@ import { EpisodesService } from './episodes.service';
 export class EpisodesController {
   constructor(private episodesService: EpisodesService) {}
 
+  @Get('/:content_id/:episode_id')
+  getEpisodeById(
+    @Param('content_id') content_id: string,
+    @Param('episode_id') episode_id: string,
+  ): Episode {
+    return this.episodesService.getEpisodeById(content_id, episode_id);
+  }
+
   @Patch('/buy/:content_id/:episode_id')
   buyEpisode(
     @Param('content_id') content_id: string,
