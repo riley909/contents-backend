82ebd6cde5697bfa904afc4f6c209c682bffe277
riley909 <increaseblue666@gmail.com>
2021-10-12 21:07:51 +0900

    add buyEpisode

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
+import { Episode } from 'contents-backend';
+import { EpisodesService } from './episodes.service';
 
 @Controller('episodes')
-export class EpisodesController {}
+export class EpisodesController {
+  constructor(private episodesService: EpisodesService) {}
+
+  @Patch('/buy/:content_id/:episode_id')
+  buyEpisode(
+    @Param('content_id') content_id: string,
+    @Param('episode_id') episode_id: string,
+  ): Episode {
+    return this.episodesService.buyEpisode(content_id, episode_id);
+  }
+}
