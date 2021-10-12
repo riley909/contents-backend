24f96b0ee9f7f9e324ea1c4215d3ee0f750f0f59
riley909 <increaseblue666@gmail.com>
2021-10-12 19:45:58 +0900

    create EpisodesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentsModule } from './contents/contents.module';
+import { EpisodesModule } from './episodes/episodes.module';
 
 @Module({
-  imports: [ContentsModule],
+  imports: [ContentsModule, EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
 })
