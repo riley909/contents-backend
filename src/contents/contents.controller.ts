bd10de7a0cb857c9e98829b0d4dff376dedd7973
riley909 <increaseblue666@gmail.com>
2021-10-12 17:21:20 +0900

    add getContentById

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { Content } from 'contents-backend';
 import { ContentsService } from './contents.service';
 import { GetContentsFilterDto } from './dto/get-contents-filter.dto';
@@ -10,5 +10,10 @@ export class ContentsController {
   @Get()
   getContents(@Query() filterDto: GetContentsFilterDto): Content[] {
     return this.contentsService.getContents(filterDto);
+  }
+
+  @Get('/:id')
+  getContentById(@Param('id') id: string): Content {
+    return this.contentsService.getContentById(id);
   }
 }
