1958333291b01cc5b01d5fd6e00b4b7cbc66a796
riley909 <increaseblue666@gmail.com>
2021-10-12 17:10:39 +0900

    add getContents

--- src/contents/contents.service.ts
+++ src/contents/contents.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Content } from 'contents-backend';
+import { GetContentsFilterDto } from './dto/get-contents-filter.dto';
+import { data } from 'contents-backend';
 
 @Injectable()
-export class ContentsService {}
+export class ContentsService {
+  contents: Content[] = data;
+
+  getContents(filterDto: GetContentsFilterDto): Content[] {
+    const { id, title } = filterDto;
+    let contents = this.contents;
+
+    if (id) {
+      contents = contents.filter((content) => content.id === id);
+    }
+
+    if (title) {
+      contents = contents.filter((content) => content.title === title);
+    }
+
+    return contents;
+  }
+}
