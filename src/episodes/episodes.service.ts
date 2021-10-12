93ba092c38067e421d503d7fd873ee0c34c40e22
riley909 <increaseblue666@gmail.com>
2021-10-12 21:01:59 +0900

    add getEpisodeById

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Episode } from 'contents-backend';
+import { ContentsService } from 'src/contents/contents.service';
 
 @Injectable()
-export class EpisodesService {}
+export class EpisodesService {
+  constructor(private contentsService: ContentsService) {}
+
+  getEpisodeById(content_id: string, episode_id: string): Episode {
+    const content = this.contentsService.getContentById(content_id);
+    const episode = content.episodes.find(
+      (episode) => episode.id === episode_id,
+    );
+
+    return episode;
+  }
+}
