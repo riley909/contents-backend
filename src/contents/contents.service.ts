ea558bd5bde392df8e9e59de7bb8148b0b28df42
riley909 <increaseblue666@gmail.com>
2021-10-12 18:10:19 +0900

    add updateContent

--- src/contents/contents.service.ts
+++ src/contents/contents.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Content } from 'contents-backend';
 import { GetContentsFilterDto } from './dto/get-contents-filter.dto';
 import { data } from 'contents-backend';
+import { UpdateContentDto } from './dto/update-content.dto';
 
 @Injectable()
 export class ContentsService {
@@ -32,5 +33,25 @@ export class ContentsService {
     }
 
     return found;
+  }
+
+  updateContent(id: string, updateContentDto: UpdateContentDto): Content {
+    const { title, image, like, summary } = updateContentDto;
+    const content = this.getContentById(id);
+
+    if (title) {
+      content.title = title;
+    }
+    if (image) {
+      content.image = image;
+    }
+    if (like) {
+      content.like = like;
+    }
+    if (summary) {
+      content.summary = summary;
+    }
+
+    return content;
   }
 }
