8fd2c510671c3101848e76e4648eab67f707c0b0
riley909 <increaseblue666@gmail.com>
2021-10-12 21:07:18 +0900

    add ContentsService

--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ContentsService } from 'src/contents/contents.service';
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
 
 @Module({
   controllers: [EpisodesController],
-  providers: [EpisodesService],
+  providers: [EpisodesService, ContentsService],
 })
 export class EpisodesModule {}
