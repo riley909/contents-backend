890945fe9a986b360f02350b28534f153cccd232
riley909 <increaseblue666@gmail.com>
2021-10-12 17:10:48 +0900

    add getContents

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Content } from 'contents-backend';
 import { ContentsService } from './contents.service';
+import { GetContentsFilterDto } from './dto/get-contents-filter.dto';
 
 @Controller('contents')
 export class ContentsController {
   constructor(private contentsService: ContentsService) {}
+
+  @Get()
+  getContents(@Query() filterDto: GetContentsFilterDto): Content[] {
+    return this.contentsService.getContents(filterDto);
+  }
 }
