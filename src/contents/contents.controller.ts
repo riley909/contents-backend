982a4313208518b232ae34c018919357480e40fc
riley909 <increaseblue666@gmail.com>
2021-10-12 11:03:48 +0900

    inject ContentsService

--- src/contents/contents.controller.ts
+++ src/contents/contents.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ContentsService } from './contents.service';
 
 @Controller('contents')
-export class ContentsController {}
+export class ContentsController {
+  constructor(private contentsService: ContentsService) {}
+}
